61688fce1ac4a054624c95ab808fd1ed9b4a90e8
liumin1128 <970568830@qq.com>
2017-10-07 22:53:24 +0800

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+
+const handler = createHandler({ path: '/webhook', secret: 'myhashsecret' });
+
+http.createServer((req, res) => {
+  handler(req, res, (err) => {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+}).listen(7777);
+
+handler.on('error', (err) => {
+  console.error('Error:', err.message);
+});
+
+handler.on('push', (event) => {
+  console.log('event');
+  console.log(event);
+  console.log(
+    'Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref,
+  );
+});
+
+handler.on('issues', (event) => {
+  console.log(
+    'Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title,
+  );
+});
