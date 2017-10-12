42f7725b0a198361b3a361f3a9e093ec50b4533e
liumin1128 <970568830@qq.com>
2017-10-12 20:20:05 +0800

    gx1

---
+++ api.react.mobi.sh
@@ -0,0 +1,9 @@
+PRO_DIR="../api.react.mobi"
+echo "start--------------------"
+cd $PRO_DIR
+echo "cd $PRO_DIR"
+echo "pull git code"
+git pull
+echo "restart nodeblog"
+# pm2 restart nodeblog
+# echo "finished-----------------"
--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const http = require('http');
-const createHandler = require('github-webhook-handler');
-
-const handler = createHandler({ path: '/webhook', secret: 'myhashsecret' });
-
-http.createServer((req, res) => {
-  handler(req, res, (err) => {
-    res.statusCode = 404;
-    res.end('no such location');
+function RunCmd(cmd, args, cb) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var result = '';
+  child.stdout.on('data', function(data) {
+    result += data.toString();
   });
-}).listen(7777);
+  child.stdout.on('end', function() {
+    cb(result)
+  });
+  child.stderr.on('data', function(data) {
+      console.log('Error: \n'+ data);
+  });
+  child.on('exit',function(code,signal) {
+      console.log('Exit: '+code);
+  });
+}
 
-handler.on('error', (err) => {
-  console.error('Error:', err.message);
-});
 
-handler.on('push', (event) => {
-  console.log('event');
-  console.log(event);
-  console.log(
-    'Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref
-  );
-});
+function test() {
+  var shpath = './api.react.mobi.sh';
+  RunCmd('sh', [shpath], function(result) {
+      console.log(result);
+  })
+}
 
-handler.on('issues', (event) => {
-  console.log(
-    'Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title
-  );
-});
+test()
