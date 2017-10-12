2ca227db100040a137e1ef694f400770ecb659a6
liumin1128 <970568830@qq.com>
2017-10-12 21:44:26 +0800

    更新·

--- auto_build.sh
+++ auto_build.sh
@@ -5,5 +5,5 @@ echo "cd $PRO_DIR"
 echo "pull git code"
 git pull
 echo "restart nodeblog"
-# pm2 restart nodeblog
-# echo "finished-----------------"
+pm2 restart webhook
+echo "finished-----------------"
